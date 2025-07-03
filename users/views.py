b7d966b9638ea7ef3150bdacbc3985e9d2cfbfa9
Marek <tesiorowski.marek@gmail.com>
2025-07-03 23:44:17 +0200

    permission to register updated

--- users/views.py
+++ users/views.py
@@ -1,4 +1,5 @@
 # from django.shortcuts import render
+from rest_framework.permissions import AllowAny
 from rest_framework import generics
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer
@@ -7,3 +8,4 @@ from .serializers import RegisterSerializer
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
+    permission_classes = [AllowAny]
