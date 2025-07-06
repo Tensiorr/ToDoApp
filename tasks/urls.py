c84bca3237caf6141eb0860dbe6847f6d1dbe38d
Marek <tesiorowski.marek@gmail.com>
2025-07-06 21:47:27 +0200

    task edit added

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, TagViewSet, tasks_list, add_task
+from .views import TaskViewSet, TagViewSet, tasks_list, add_task, edit_task
 
 router = DefaultRouter()
 router.register(r"tasks", TaskViewSet, basename="task")
@@ -10,4 +10,5 @@ urlpatterns = [
     path("", include(router.urls)),
     path("list/", tasks_list, name="tasks_list"),
     path("add/", add_task, name="add_task"),
+    path('tasks/edit/<int:task_id>/', edit_task, name='edit_task'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -3,7 +3,7 @@ from rest_framework import viewsets, permissions, filters
 from .models import Task, Tag
 from .serializers import TaskSerializer, TagSerializer
 from django_filters.rest_framework import DjangoFilterBackend
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .forms import TaskForm
 
@@ -75,3 +75,30 @@ def add_task(request):
         form.fields["existing_tags"].queryset = Tag.objects.filter(user=request.user)
 
     return render(request, "add_task.html", {"form": form})
+
+
+def edit_task(request, task_id):
+    task = get_object_or_404(Task, id=task_id, user=request.user)
+
+    if request.method == "POST":
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task = form.save(commit=False)
+            task.save()
+
+            existing_tags = form.cleaned_data["existing_tags"]
+            task.tags.set(existing_tags)
+
+            new_tags_str = form.cleaned_data["new_tags"]
+            new_tag_names = [
+                name.strip() for name in new_tags_str.split(",") if name.strip()
+            ]
+            for tag_name in new_tag_names:
+                tag, _ = Tag.objects.get_or_create(name=tag_name, user=request.user)
+                task.tags.add(tag)
+
+            return redirect("tasks_list")
+    else:
+        initial = {"existing_tags": task.tags.all(), "new_tags": ""}
+        form = TaskForm(instance=task, initial=initial)
+    return render(request, "tasks/edit_task.html", {"form": form, "task": task})
